2dd92b2578f17a2131ccebfb55cdccab51894d6b
Tatok-n <tnhunter2004@gmail.com>
2025-01-31 18:49:20 -0500

    started work on some frontend stuff :D

---
+++ beanboi_frontend/lib/widgets/displayUtils/userPrefs.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Userprefs {
+  bool darkTheme = true;
+  Color mainAccent = Color.fromARGB(255, 255, 181, 23);
+  Color accent2 = Color.fromARGB(255, 255, 181, 23);
+  Color accent3 = Color.fromARGB(255, 255, 181, 23);
+  
+}
