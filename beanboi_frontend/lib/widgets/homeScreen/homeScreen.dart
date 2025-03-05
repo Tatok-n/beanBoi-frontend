4ef7b4caffa65dbafe62a92195384bf7529d9059
Tatok-n <tnhunter2004@gmail.com>
2025-03-05 00:50:52 -0500

    forgor to push oops

--- beanboi_frontend/lib/widgets/homeScreen/homeScreen.dart
+++ beanboi_frontend/lib/widgets/homeScreen/homeScreen.dart
@@ -1,15 +1,10 @@
 import 'package:beanboi_frontend/widgets/beansDisplay/beanDisplay.dart';
+import 'package:beanboi_frontend/widgets/commonUtils/navBar.dart';
 import 'package:flutter/material.dart';
 import 'package:beanboi_frontend/widgets/commonUtils/userPrefs.dart';
 
 class HomeScreen extends StatelessWidget {
   Userprefs prefs = Userprefs();
-  List<Map<String, dynamic>> cardNames = [{'name' : 'Beans', 'route' : _routeToBeans()},
-  {'name' : 'Bean Purchases', 'route' : _routeToBeans()},
-  {'name' : 'Grinders', 'route' : _routeToBeans()},
-  {'name' : 'Recipes', 'route' : _routeToBeans()},
-  {'name' : 'Brews', 'route' : _routeToBeans()},
-  {'name' : 'Stats', 'route' : _routeToBeans()}];
 
 
 
@@ -17,11 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    int screenIndex = 0;
 
-      void handleScreenChanged(int selectedScreen) {
-    Navigator.of(context).push(cardNames[selectedScreen]["route"]);
-  }
 
     return Scaffold(
       backgroundColor: Colors.transparent,
@@ -37,39 +28,8 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      drawer:  NavigationDrawer(
-        onDestinationSelected: handleScreenChanged,
-        selectedIndex: screenIndex,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
-            child: Text('Header', style: Theme.of(context).textTheme.titleSmall),
-          ),
-          ...cardNames.map((Map destination) {
-            return NavigationDrawerDestination(
-              label: Text(destination['name']),
-              icon: Icon(Icons.abc),
-              selectedIcon:  Icon(Icons.abc),
-            );
-          }),
-          const Padding(padding: EdgeInsets.fromLTRB(28, 16, 28, 10), child: Divider()),
-        ],
-      )
+      drawer:  navBar(),
     );
   }
 }
 
-Route _routeToBeans() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => BeansDisplay(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(0.0, 1.0);
-      const end = Offset.zero;
-      const curve = Curves.easeInCirc;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(position: animation.drive(tween), child: child);
-    },
-  );
-}
