b72d8b2077af8d7513e7a50d03e2600572e1c1c2
Tatok-n <tnhunter2004@gmail.com>
2025-03-17 15:57:57 -0400

    added localStorage :D

--- beanboi_frontend/lib/controllers/beanCaller.dart
+++ beanboi_frontend/lib/controllers/beanCaller.dart
@@ -1,13 +1,19 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:beanboi_frontend/widgets/PlasmaBackgound/plasma.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'httpManager.dart' as manager;
 
 Future<List<Map<String,dynamic>>> getAllBeans(String UID) async {
+  final prefs = await SharedPreferences.getInstance();
   String getAllBeansURL = "/users/"+UID+"/beans"; 
-  List<dynamic> data = jsonDecode(await manager.sendGet(getAllBeansURL));
+  String jsonValues = await manager.sendGet(getAllBeansURL);
+  List<dynamic> data = jsonDecode(jsonValues);
   List<Map<String, dynamic>> fetchedBeans = data.cast<Map<String, dynamic>>();
-  return fetchedBeans ;
+  await prefs.setString('beans', jsonValues);
+  return fetchedBeans;
 }
 
 Future<void> saveBean (Map<String, dynamic> map, String UID) async {
